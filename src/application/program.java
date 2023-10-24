14c0e81bfefe3e8faf0bb44954e6697108b81bdc
Victor Hugo <victorzk6@hotmail.com>
2023-10-24 14:04:39 -0300

    teste

--- src/application/program.java
+++ src/application/program.java
@@ -1,12 +1,24 @@
 package application;
 import chess.ChessMatch;
+import chess.ChessPosition;
 
 public class program {
 
 	public static void main(String[] args) {
 		
 		ChessMatch chessMatch = new ChessMatch();
+		
+		while (true) {
 		UI.printBoard((chessMatch.getPieces()));
+		System.out.println();
+		System.out.print("Source: ");
+		ChessPosition source = UI.readChessPosition(sc);
+		
+		System.out.println();
+		System.out.print("target: ");
+		ChessPosition target = UI.readChessPosition(sc);
+		
+		ChessPiece capturedPiece = chessMatch.perfomChessMove(source, target);
 
 	}
 
